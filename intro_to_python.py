87708db46d2c4c95676c6449617dc3228b8958f8
josephiznot <josephiznot@gmail.com>
2018-12-04 22:29:41 -0600

    finished

--- intro_to_python.py
+++ intro_to_python.py
@@ -3,10 +3,11 @@
 #####################
 #######_Intro_#######
 #####################
+#  The 'print' method, prints the output to the screen.
 
-#  Step 1: Print 'Hello world' to the console. 
+#  Step: Print 'Hello world' to the screen. 
 
-#  Step 2: Comment out the line above line of code.
+#  Step: Comment out the line above line of code.
 
 #####################
 #####_Variables_#####
@@ -14,28 +15,25 @@
 #  Variables store data for values. They can be of many types, such as
 #  a string, a number, a boolean(True, False), or even a calulation.
 
-#  Step 1: Create a variable for your first name and last name.
-first_name = 'Joe'
-last_name = 'Anderson'
+#  Step: Create a variable for your first name and last name.
 
-#  Step 2: Create a few more variables describing characteristics of yourself.
-age = 22
-height = 6.16
-hair_color= 'blonde'
+#  Step: Create a few more variables describing characteristics of yourself.
+
+#  The print method can print multiple values outputs at once.
+#  Step: Print the characteristics from above:
 
 #  Variables can even store results for arithmetic problems.
-#  Step 3: Calculate the dog's age in dog years and store it in a variable.
+#  Step: Calculate the dog's age in dog years and store it in a variable.
 #          Hint: 1 year = 7 dog years.
-dogs_age = 1.5
-dog_years= dogs_age * 7
+dogs_age = 1
 
 #####################
 #######_Lists_#######
 #####################
 #  A list is an indexed pair of values starting with 0.
+#  Its values are denoted in square brackets e.g. ['value1', 'value2']
 
-#  Step: Create a list of 5 video games.
-friends = [friend1, friend2, friend3]
+#  Step: Create a list of 5 video games and assign it to a variable.
 
 #  You can access a value in a list by using bracket notation with its index.
 #  Example:
@@ -47,7 +45,8 @@ friends = [friend1, friend2, friend3]
 
 #  You can change values by simply assigning it again..
 #  E.g. friends[0] = 'Bill'
-#  Step: Change a video game's name in the list.
+
+#  Step: Change a video game's name in the list and then print the entire list.
 
 #####################
 #####_Functions_#####
@@ -62,13 +61,13 @@ friends = [friend1, friend2, friend3]
 #        height = 3
 #        length = 5
 
-#  Step: Create a function that calculates the volume of a figure.
+#  Step: Create a function that calculates the volume of a figure and then print the result.
 
-#####################
-#####_Conditions_####
-#####################
-#  Python allows you to compare two values using conditional operators
-#  such as '==', '!=', '<', '>', '>=', '<='.
+##########################
+###Comparison Operators###
+##########################
+#  Python allows you to compare two values using comparision operators
+#  such as '==', '!=', '<', '>', '>=', '<='. Conditions will evaluate to either True or False.
 
 #  Step: Calculate the volumes of two figures and then compare them.
 #        Print the conditional statement.
@@ -77,15 +76,14 @@ friends = [friend1, friend2, friend3]
 ########_IF_#########
 #####################
 #  The 'if' statement is a boolean expression that executes a block
-#  of code if the condition evaluates to True. 
+#  of code if the conditional statement evaluates to True. 
 
 #  Step: If the two figures do not equal each other from above,
 #        print 'Not equal to each other.' to the console.
 
 #  The 'else' statement comes after the 'if' statement, and will execute
-#  when the previous condition fails. Think of it like the catch all statement.
+#  when the previous conditions fail. Think of it like the catch-all statement.
 
 #  Step: If the two figures do each other, print 'Equal to each other.'
-
 
 

3eb366d0fef7f1ad74cb41d850f81fd58beb56ea
josephiznot <josephiznot@gmail.com>
2018-12-04 21:59:45 -0600

    Initial commit

---
+++ intro_to_python.py
@@ -0,0 +1,91 @@
+#  Welcome to Python!
+
+#####################
+#######_Intro_#######
+#####################
+
+#  Step 1: Print 'Hello world' to the console. 
+
+#  Step 2: Comment out the line above line of code.
+
+#####################
+#####_Variables_#####
+#####################
+#  Variables store data for values. They can be of many types, such as
+#  a string, a number, a boolean(True, False), or even a calulation.
+
+#  Step 1: Create a variable for your first name and last name.
+first_name = 'Joe'
+last_name = 'Anderson'
+
+#  Step 2: Create a few more variables describing characteristics of yourself.
+age = 22
+height = 6.16
+hair_color= 'blonde'
+
+#  Variables can even store results for arithmetic problems.
+#  Step 3: Calculate the dog's age in dog years and store it in a variable.
+#          Hint: 1 year = 7 dog years.
+dogs_age = 1.5
+dog_years= dogs_age * 7
+
+#####################
+#######_Lists_#######
+#####################
+#  A list is an indexed pair of values starting with 0.
+
+#  Step: Create a list of 5 video games.
+friends = [friend1, friend2, friend3]
+
+#  You can access a value in a list by using bracket notation with its index.
+#  Example:
+#  friends = ['Sam', 'John', 'Liz']
+#  print(friends[0])
+#  >>'Liz'
+
+#  Step:  Print the 3rd game in the list of games.
+
+#  You can change values by simply assigning it again..
+#  E.g. friends[0] = 'Bill'
+#  Step: Change a video game's name in the list.
+
+#####################
+#####_Functions_#####
+#####################
+#  Step: Calculate the volume of a figure for given dimensions:
+#        width = 4
+#        height = 4
+#        length = 12
+
+#  Step: Do it again, but with these dimensions:
+#        width = 2
+#        height = 3
+#        length = 5
+
+#  Step: Create a function that calculates the volume of a figure.
+
+#####################
+#####_Conditions_####
+#####################
+#  Python allows you to compare two values using conditional operators
+#  such as '==', '!=', '<', '>', '>=', '<='.
+
+#  Step: Calculate the volumes of two figures and then compare them.
+#        Print the conditional statement.
+
+#####################
+########_IF_#########
+#####################
+#  The 'if' statement is a boolean expression that executes a block
+#  of code if the condition evaluates to True. 
+
+#  Step: If the two figures do not equal each other from above,
+#        print 'Not equal to each other.' to the console.
+
+#  The 'else' statement comes after the 'if' statement, and will execute
+#  when the previous condition fails. Think of it like the catch all statement.
+
+#  Step: If the two figures do each other, print 'Equal to each other.'
+
+
+
